b51884fe8d999bf55997ff41a3422b3838e1ee30
Kamalesh T <kamleshthangkarasu@gmail.com>
2026-09-02 07:25:49 +0530

    Time: 0 ms (100%), Space: 30.3 MB (36.43%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
-        unordered_set<int>st;
+        //unordered_set<int>st;
+        bool isOdd=false;
+        bool isEven=false;
         for(int i=0;i<nums1.size();i++)
         {
-            st.insert(nums1[i]%2);
+            //st.insert(nums1[i]%2);
+            if(nums1[i]%2==0)
+            isEven=true;
+            else 
+            isOdd=true;
         }
         bool odd=true;
         bool even=true;
         for(int i=0;i<nums1.size();i++)
         {
-            if(nums1[i]%2==0 && !st.count(1))
+            if(nums1[i]%2==0 && !isOdd)
             {
                 odd=false;
                 break;
@@ -18,7 +24,7 @@ public:
         }
         for(int i=0;i<nums1.size();i++)
         {
-            if(nums1[i]%2==1 && !st.count(0))
+            if(nums1[i]%2==1 && ! isEven)
             {
                 even=false;
                 break;
