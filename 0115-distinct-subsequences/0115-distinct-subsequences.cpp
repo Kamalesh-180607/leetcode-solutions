21037c55894014abbd92a603fa5aa5f9e0b0267a
Kamalesh T <kamleshthangkarasu@gmail.com>
2026-09-06 08:02:40 +0530

    Time: 2722 ms (6.41%), Space: 422.2 MB (5.1%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -0,0 +1,64 @@
+class Solution {
+public:
+    int solve(string s, string t,int i,int j,vector<vector<int>>& dp)
+    {
+        if(j<0)return 1;
+        if(i<0)return 0;
+        if(dp[i][j]!=-1)return dp[i][j];
+        if(s[i]==t[j])
+        return dp[i][j]=solve(s,t,i-1,j-1,dp)+solve(s,t,i-1,j,dp);
+
+        return dp[i][j]=solve(s,t,i-1,j,dp);
+    }
+
+    //EFFICIENT SOLUTION
+
+//     class Solution {
+// public:
+//     int numDistinct(string s, string t) {
+//         int m = s.length();
+//         int n = t.length();
+
+//         vector<unsigned long long> prev(n + 1, 0);
+//         prev[0] = 1;   // Empty string is always a subsequence
+
+//         for (int i = 1; i <= m; i++) {
+//             // Traverse backwards
+//             for (int j = n; j >= 1; j--) {
+//                 if (s[i - 1] == t[j - 1]) {
+//                     prev[j] = prev[j] + prev[j - 1];
+//                 }
+//             }
+//         }
+
+//         return (int)prev[n];
+//     }
+// };
+    int numDistinct(string s, string t) {
+        int m=s.length();
+        int n=t.length();
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+        return solve(s,t,m-1,n-1,dp);
+        //vector<vector<long long>> dp(m+1,vector<long long>(n+1,0));
+        for(int i=0;i<=m;i++)
+        {
+            dp[i][0]=1;
+        }
+        for(int j=1;j<=n;j++)
+        {
+            dp[0][j]=0;
+        }
+        for(int i=1;i<=m;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                if(s[i-1]==t[j-1])
+                dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+                else
+                dp[i][j]=dp[i-1][j];
+            }
+        }
+        return dp[m][n];
+        
+    }
+};
